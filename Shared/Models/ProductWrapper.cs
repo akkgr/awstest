47a1d9f7f0c8a7d3ae75b4da37eed9098c1a847d
akkggr <akkgr@users.noreply.github.com>
2024-01-29 20:32:09 +0200

    init

---
+++ Shared/Models/ProductWrapper.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Shared.Models
+{
+    public class ProductWrapper
+    {
+        [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(ProductWrapper))]
+        public ProductWrapper()
+        {
+            this.Products = new List<Product>();
+        }
+
+        public ProductWrapper(List<Product> products)
+        {
+            this.Products = products;
+        }
+
+        public List<Product> Products { get; set; }
+    }
+}
